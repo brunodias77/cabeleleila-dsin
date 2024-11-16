afe5fe6558c3d614a37029bb1bce205689483e3b
brunodias77 <brunohdias95@gmail.com>
2024-11-16 09:23:59 -0300

    first commit

---
+++ dsin-cabeleleila-server/src/main/java/com/brunodias/dsin_cabeleleila_server/DsinCabeleleilaServerApplication.java
@@ -0,0 +1,13 @@
+package com.brunodias.dsin_cabeleleila_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsinCabeleleilaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsinCabeleleilaServerApplication.class, args);
+	}
+
+}
---
+++ dsin-cabeleleila-server/src/test/java/com/brunodias/dsin_cabeleleila_server/DsinCabeleleilaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunodias.dsin_cabeleleila_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsinCabeleleilaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
